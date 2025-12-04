2c6c959f9ef351c851d733ddd354a7783961f3fa
cape <cape06.hk@gmail.com>
2025-12-04 16:21:58 +0100

    Remove useless import

--- src/main/java/net/sixchan/relics/item/custom/NeedleItem.java
+++ src/main/java/net/sixchan/relics/item/custom/NeedleItem.java
@@ -1,19 +1,6 @@
 package net.sixchan.relics.item.custom;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.world.World;
-import net.sixchan.relics.block.ModBlocks;
-
-import java.util.Map;
 
 public class NeedleItem extends Item {
     public NeedleItem(Settings settings) {
